4745f1074e67c799e661cbc43672d3080d239714
ponmuthu5990 <30725846+ponmuthu5990@users.noreply.github.com>
2018-05-17 15:35:01 +0530

    Handle unauthorized response
    Add Register and Login UI

--- angular5Auth/src/app/app.module.ts
+++ angular5Auth/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
-
+import { AuthService } from './auth.service';
+import { EventService } from './event.service';
+import { AuthGuard } from './auth.guard';
+import { TokenInterceptorService } from './token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -20,9 +24,16 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, EventService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular5Auth/src/app/special-events/special-events.component.ts
+++ angular5Auth/src/app/special-events/special-events.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-special-events',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialEventsComponent implements OnInit {
 
-  constructor() { }
+  specialEvents = [];
+
+  constructor(private _eventService: EventService, private _router: Router) { }
+
 
   ngOnInit() {
+    this._eventService.getSpecialEvents()
+      .subscribe(
+        res => this.specialEvents = res,
+        err => {
+          if ( err instanceof HttpErrorResponse ) {
+            if (err.status === 401) {
+              this._router.navigate(['/login']);
+            }
+          }
+        }
+      );
   }
 
 }
